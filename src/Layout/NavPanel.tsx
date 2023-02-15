ada885d0d30a63b576affc46a6f8ddb11954bbbb
cnielson912 <cnielson912@gmail.com>
2023-02-15 13:08:06 -0600

    add user modal styling

--- src/Layout/NavPanel.tsx
+++ src/Layout/NavPanel.tsx
@@ -4,7 +4,7 @@ export default function NavPanel(){
     const navigate = useNavigate()
     return(
         <div className="space-y-3">
-            <text className="text-3xl w-[100%] outline">Nav Panel</text>
+            <text className="text-3xl w-[100%]">Nav Panel</text>
             <button className="outline bg-[#2ed7c3] h-[40px] w-[100%] rounded" onClick={()=>navigate('/Dashboard')}>Dashboard</button>
             <button className="outline bg-[#2ed7c3] h-[40px] w-[100%] rounded" onClick={()=>navigate('/Admin')}>Admin</button>
         </div>
--- src/Pages/AdminPage.tsx
+++ src/Pages/AdminPage.tsx
@@ -34,10 +34,20 @@ export default function AdminPage(){
                     onRequestClose={closeModal}
                     style={modalStyle}
                 >
-                    <div>
+                    <div className="space-x-3">
                         <text className="text-3xl">Add User</text>
-                        <button onClick={closeModal}>close</button>
-                        <div>I am a modal</div>
+                        <br/>
+                        <text>Company:</text>
+                        <input className="outline rounded"/>
+                        <text>Name:</text>
+                        <input className="outline rounded"/>
+                        <text>Email:</text>
+                        <input className="outline rounded"/>
+                        <text>Password:</text>
+                        <input className="outline rounded"/>
+                        <button className="rounded h-[40px] w-[80px] bg-lime-600">Submit</button>
+                        <button className="rounded h-[40px] w-[80px] bg-red-600" onClick={closeModal}>Close</button>
+                        
                     </div>
                     
                 </Modal>
