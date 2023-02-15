369f0014af1fc1aff75487132a6fe1104757c9d7
cnielson912 <cnielson912@gmail.com>
2023-02-15 17:01:44 -0600

    I *think* the logo should scale with screen size now

--- src/Layout/Header.tsx
+++ src/Layout/Header.tsx
@@ -4,8 +4,10 @@ export default function Header(){
   const logo = 'https://static.wixstatic.com/media/4f8b60_2899998071014662a534db34be03a3d1~mv2.png/v1/fill/w_438,h_118,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/Integrity-Logo_2x%20(3)_edited.png'
   const navigate = useNavigate();
   return(
-  <div className="mr-[1%] ml-[1%] flex">
-    <img src={logo}/>
+  <div className="mr-[1%] ml-[1%] h-80px flex">
+    <div className="flex min-h-[100%] h-auto justify-start self-start">
+      <img className="min-h-full h-auto" src={logo}/>
+    </div>
     <div className="flex w-[100%] justify-end self-end">
     <button className="bg-[#21345b] text-white h-[40px] w-[80px] rounded outline" onClick={()=>navigate('/Login')}>Log in</button>
     </div>
