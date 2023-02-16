896fc5f395490895588c523ffeb7589320a82e1a
Ryan Boyce <86334509+ryan-boyce@users.noreply.github.com>
2023-02-16 16:31:15 -0600

    Add assertion to integ. test

--- cypress/e2e/AddUserSpec.cy.ts
+++ cypress/e2e/AddUserSpec.cy.ts
@@ -7,15 +7,17 @@ describe('add user spec', () => {
   })
 
   specify('add a new user', () => {
-    cy.get('.m-\\[2\\%\\] > :nth-child(3)').click();
+    cy.get(':nth-child(2) > .m-\\[2\\%\\] > :nth-child(3)').click();
 
     cy.get('.m-\\[2\\%\\] > .justify-end > .outline').click();
-    cy.get('.space-x-3 > :nth-child(4)').type('Integrity Inspired Solutions');
+    cy.get('.space-x-3 > :nth-child(4)').type('0');
     cy.get('.space-x-3 > :nth-child(6)').type('Mr. Test');
     cy.get('.space-x-3 > :nth-child(8)').type('test@testing.com');
     cy.get('.space-x-3 > :nth-child(10)').type('ilovetesting');
     
     cy.get('.bg-lime-600').click();
+
+    cy.contains('Mr. Test');
   })
 
 })
