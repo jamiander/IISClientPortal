4574d8a2b40c41352f3b551d40241d5b173881a2
Ryan Boyce <86334509+ryan-boyce@users.noreply.github.com>
2023-02-16 15:06:42 -0600

    Add specification to getAllUsers

--- src/Services/UserService.tsx
+++ src/Services/UserService.tsx
@@ -2,13 +2,36 @@ import axios from "axios";
 import { User } from "../Store/UserSlice";
 import { BASE_URL, http } from "./Http";
 
+interface GetAllUsersRequest {
+    id?: number,
+    email?: string,
+    companyId?: number
+}
+
 interface GetAllUsersResponse {
     users: User[]
 }
 
-export async function GetAllUsers(): Promise<User[]>
+export async function GetAllUsers(request?: GetAllUsersRequest): Promise<User[]>
 {
     let baseUrl = BASE_URL + "GetUserBlob?code=p8O-6DDRKEKkyBLCUW2dFAkBtDZj45u_tCVkRhQRhQFbAzFuq_N7Kg=="//"https://clientportaltestfunction.azurewebsites.net/api/Users?code=7Pala2sxQeLXQT9C5q8Coj2H_JbOW_4tzuUYbxCB7fiYAzFu5f8hZw=="//BASE_URL + "Users";
+
+    let query = [];
+    if(request)
+    {
+        if(request.id !== undefined)
+            query.push(`id=${request.id}`);
+        if(request.email !== undefined)
+            query.push(`email=${request.email}`);
+        if(request.companyId !== undefined)
+            query.push(`companyId=${request.companyId}`);
+
+        if(query.length > 0)
+        {
+            baseUrl += "&"; //the function code's already a part of the query
+            baseUrl += query.join("&");
+        }
+    }
 
     let response = await axios.get(baseUrl)//http.get<GetAllUsersResponse>(baseUrl);
     //console.log(response)
