1f640d7d82213587cfeaff2448f387a6726155ed
cnielson912 <cnielson912@gmail.com>
2023-02-16 15:25:03 -0600

    added a select on dashboard for choosing between initiatives

--- src/Pages/DashboardPage.tsx
+++ src/Pages/DashboardPage.tsx
@@ -5,9 +5,17 @@ export default function DashboardPage(){
   const dispatch = useAppDispatch();
     
   return(
-    <div className="m-[2%]">
-      <p className="text-5xl">DashBoard</p>
-      <button className="bg-[#21345b] h-[40px] w-[80px] rounded" onClick={() => {dispatch(getUserData())}}></button>
+    <div className="m-[2%] grid grid-cols-4">
+      <div className="col-span-2">
+        <p className="text-5xl">DashBoard</p>
+        <button className="bg-[#21345b] h-[40px] w-[80px] rounded" onClick={() => {dispatch(getUserData())}}></button>
+      </div>
+      <div className="flex justify-end col-span-2">
+        <select className="outline rounded w-[200px] h-[40px]">
+          <option>Select Initiative</option>
+        </select>
+      </div>
+      
     </div>
   )
 }
