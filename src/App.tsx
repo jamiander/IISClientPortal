ad7d28ba61e51fad633a3cc384b39dbcebd087ae
cnielson912 <cnielson912@gmail.com>
2023-02-17 14:30:17 -0600

    minor change to make sure page gets bigger to fit more content

--- src/App.tsx
+++ src/App.tsx
@@ -16,8 +16,8 @@ function App() {
   return (
     <div className='grid grid-cols-5 gap-[.5vh] bg-[#2ed7c3]'>
       <div className='col-span-5 bg-white h-30px'><Header/></div>
-      <div className='bg-white h-[80vh]'><NavPanel/></div>
-      <div className='col-span-4 bg-white h-[80vh]'><Content/></div>
+      <div className='bg-white h-auto'><NavPanel/></div>
+      <div className='col-span-4 bg-white min-h-[80vh] h-auto'><Content/></div>
       <div className='col-span-5 bg-[#21345b] text-white h-[9vh]'><Footer/></div>
     </div>
   );
