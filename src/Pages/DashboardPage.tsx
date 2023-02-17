c52024ba99d802f229e3e37b220a41322c6a41f3
cnielson912 <cnielson912@gmail.com>
2023-02-17 15:42:54 -0600

    initiatives should map to the selector on dashboard

--- src/Pages/DashboardPage.tsx
+++ src/Pages/DashboardPage.tsx
@@ -1,8 +1,12 @@
-import { useAppDispatch } from "../Store/Hooks"
-import { getUserData } from "../Store/UserSlice";
+import { selectAllCompanies } from "../Store/CompanySlice";
+import { useAppDispatch, useAppSelector } from "../Store/Hooks"
+import { selectCurrentUser } from "../Store/UserSlice";
 
 export default function DashboardPage(){
   const dispatch = useAppDispatch();
+  const user = useAppSelector(selectCurrentUser);
+  const companyList = useAppSelector(selectAllCompanies);
+  const company = companyList.find(e=>e.id === user?.companyId);
     
   return(
     <div className="m-[2%] grid grid-cols-4">
@@ -12,6 +16,11 @@ export default function DashboardPage(){
       <div className="flex justify-end col-span-2">
         <select className="outline rounded w-[200px] h-[40px]">
           <option>Select Initiative</option>
+          {company?.initiatives?.map((initiative)=>{
+            return(
+              <option>{initiative}</option>
+            )
+          })}
         </select>
       </div>
       <div className="col-span-4 h-[60vh] outline">
