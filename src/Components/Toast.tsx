ff0cb0bac90df16349377f1a10911c01e286d6d0
Julian Garver <julian.garver@integrityinspired.com>
2023-02-21 17:03:14 -0600

    updated toast styles

--- src/Components/Toast.tsx
+++ src/Components/Toast.tsx
@@ -1,7 +1,5 @@
 import { useEffect, useState } from "react";
 
-
-
 export interface ToastDetails {
     message: string,
     type: 'Success' | 'Error' | 'Warning' | 'Info',
@@ -13,57 +11,61 @@ interface ToastProps {
 }
 
 export default function Toast(props: ToastProps) {
-    const [list, setList] = useState(props.toastList);
+  const [list, setList] = useState(props.toastList);
 
-    useEffect(() => {
-        setList([...props.toastList]);
-    }, [props.toastList]);
+  useEffect(() => {
+    setList([...props.toastList]);
+  }, [props.toastList]);
 
-    useEffect(() => {
-        const interval = setInterval(deleteToast, 2000);
-        console.log("interval: ", interval);
+  useEffect(() => {
+    const interval = setInterval(deleteToast, 5000);
 
-        return () => {
-            clearInterval(interval)
-        };
-    }, [props.toastList, list]);
+    return () => {
+        clearInterval(interval)
+    };
+  }, [props.toastList, list]);
 
-    function deleteToast() {
-        if(props.toastList.length)
-        {
-            const id = props.toastList[0].id;
+  function deleteToast() {
+    if(props.toastList.length)
+    {
+      const id = props.toastList[0].id;
 
-            const listItemIndex = list.findIndex(e => e.id === id);
-            list.splice(listItemIndex, 1);
+      const listItemIndex = list.findIndex(e => e.id === id);
+      list.splice(listItemIndex, 1);
 
-            const toastListItem = props.toastList.findIndex(e => e.id === id);
-            props.toastList.splice(toastListItem, 1);
+      const toastListItem = props.toastList.findIndex(e => e.id === id);
+      props.toastList.splice(toastListItem, 1);
 
-            setList([...list]);
-        }
+      setList([...list]);
     }
+  }
 
-    return (
-        <div id='toast-container'>
-            {
-                list.map((toast, index) => 
-                    <div key={index} id="toast-default" className="flex absolute bottom-10 right-5 items-center w-full max-w-xs p-4 text-gray-500 bg-white rounded-lg shadow dark:text-gray-400 dark:bg-gray-800" role="alert">
+  return (
+    <div id='toast-container'>
+      {
+        list.map((toast, index) => 
+          <div key={index} id="toast-default" className="flex absolute bottom-10 right-10 items-center w-full max-w-xs p-4 bg-white rounded-lg shadow dark:text-gray-400 dark:bg-[#21345b]" role="alert">
+  
+            <div className="inline-flex items-center justify-center flex-shrink-0 w-8 h-8 text-blue-500 bg-blue-100 rounded-lg dark:bg-[#2ed7c3] dark:text-blue-200">
+              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="black" className="w-6 h-6">
+                <path strokeLinecap="round" strokeLinejoin="round" d="M12 9v3.75m9-.75a9 9 0 11-18 0 9 9 0 0118 0zm-9 3.75h.008v.008H12v-.008z" />
+              </svg>
+            </div>
             
-                        <div className="inline-flex items-center justify-center flex-shrink-0 w-8 h-8 text-blue-500 bg-blue-100 rounded-lg dark:bg-blue-800 dark:text-blue-200">!!!</div>
-                        
-                        <div className="ml-3 text-sm font-normal">{toast.message}</div>
-        
-                        <button type="button" data-dismiss-target="#toast-default" aria-label="Close"
-                            className="ml-auto outline outline-1 -mx-1.5 -my-1.5 bg-white text-gray-400 hover:text-gray-900 rounded-lg focus:ring-2 focus:ring-gray-300 p-1.5 hover:bg-gray-100 inline-flex h-8 w-8 dark:text-gray-500 dark:hover:text-white dark:bg-gray-800 dark:hover:bg-gray-700">
-                            <span className="sr-only">Close</span>
-                            <p className="text-white">X</p>
-                        </button>
-        
-                    </div>
-                )
-            }
-        </div>
+            <div className="ml-3 text-sm font-normal text-white">{toast.message}</div>
 
+            <button type="button" data-dismiss-target="#toast-default" aria-label="Close" onClick={() => deleteToast()}
+              className="ml-auto -mx-1.5 -my-1.5 bg-white hover:text-gray-900 rounded-lg focus:ring-2 focus:ring-gray-300 p-1.5 hover:bg-gray-100 inline-flex h-8 w-8 dark:text-gray-500 dark:hover:text-white dark:bg-[#21345b] dark:hover:bg-gray-700">
+              <span className="sr-only">Close</span>
+              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+                <path strokeLinecap="round" strokeLinejoin="round" d="M6 18L18 6M6 6l12 12" />
+              </svg>
 
-    )
+            </button>
+
+          </div>
+        )
+      }
+    </div>
+  )
 }
