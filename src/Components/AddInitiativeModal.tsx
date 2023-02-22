1b8a7ccd4eee3c8fecb4105bc3e0db6b7e10fd98
Julian Garver <julian.garver@integrityinspired.com>
2023-02-22 11:27:27 -0600

    remove unnecessary files and moved styles

--- src/Components/AddInitiativeModal.tsx
+++ src/Components/AddInitiativeModal.tsx
@@ -1,26 +1,38 @@
 import Modal from 'react-modal';
-import { modalStyle } from '../App';
+import { modalStyle } from '../Styles';
 
 interface AddInitiativeProps {
-    addInitiativeIsOpen: boolean,
-    setInitiativeIsOpen: (value: boolean) => void
+  addInitiativeIsOpen: boolean,
+  setInitiativeIsOpen: (value: boolean) => void
 }
 
 export default function AddInitiativeModal(props: AddInitiativeProps) {
-    return (
-        <div className="flex justify-end">
-            <button onClick={() => props.setInitiativeIsOpen(true)} className="outline bg-[#21345b] text-white h-[100%] w-[80%] rounded-md">
-                Add Initiative
-            </button>
+  const inputStyle = "outline rounded outline-1 p-2 w-full";
 
-            <Modal
-                isOpen={props.addInitiativeIsOpen}
-                onRequestClose={() => props.setInitiativeIsOpen(false)}
-                style={modalStyle}
-                appElement={document.getElementById('root') as HTMLElement}     
-            >
+  return (
+    <div className="flex justify-end">
+      <button onClick={() => props.setInitiativeIsOpen(true)} className="outline bg-[#21345b] text-white h-[100%] w-[80%] rounded-md">
+        Add Initiative
+      </button>
 
-            </Modal>
+      <Modal
+        isOpen={props.addInitiativeIsOpen}
+        onRequestClose={() => props.setInitiativeIsOpen(false)}
+        style={{'content': {...modalStyle.content, 'width' : '25%'}}}
+        appElement={document.getElementById('root') as HTMLElement}     
+      >
+        <div className='space-x-3'>
+          <p className='text-3xl'>Add Initiative</p>
+
+          <div className='w-full'>
+            <p>Title:</p>
+            <input />
+            
+            <p>Target Date:</p>
+            <p>Total Items:</p>
+          </div>
         </div>
-    )
+      </Modal>
+    </div>
+  )
 }
--- src/Components/AddUserModal.tsx
+++ src/Components/AddUserModal.tsx
@@ -1,5 +1,5 @@
 import Modal from 'react-modal';
-import { modalStyle } from '../App';
+import { modalStyle } from '../Styles';
 import { Company } from '../Store/CompanySlice';
 
 interface AddUserProps {
