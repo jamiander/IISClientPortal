d1e21b0885a117868b1cc9645cd9c70782d6b79f
Julian Garver <julian.garver@integrityinspired.com>
2023-02-22 11:16:36 -0600

    Add initiative displays

---
+++ src/Components/AddInitiativeModal.tsx
@@ -0,0 +1,26 @@
+import Modal from 'react-modal';
+import { modalStyle } from '../App';
+
+interface AddInitiativeProps {
+    addInitiativeIsOpen: boolean,
+    setInitiativeIsOpen: (value: boolean) => void
+}
+
+export default function AddInitiativeModal(props: AddInitiativeProps) {
+    return (
+        <div className="flex justify-end">
+            <button onClick={() => props.setInitiativeIsOpen(true)} className="outline bg-[#21345b] text-white h-[100%] w-[80%] rounded-md">
+                Add Initiative
+            </button>
+
+            <Modal
+                isOpen={props.addInitiativeIsOpen}
+                onRequestClose={() => props.setInitiativeIsOpen(false)}
+                style={modalStyle}
+                appElement={document.getElementById('root') as HTMLElement}     
+            >
+
+            </Modal>
+        </div>
+    )
+}
