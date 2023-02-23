3f2d1469304b383ea14efbc872270553a1064125
cnielson912 <cnielson912@gmail.com>
2023-02-23 13:42:23 -0600

    separated month day year inputs in add initiative modal

--- src/Components/AddInitiativeModal.tsx
+++ src/Components/AddInitiativeModal.tsx
@@ -51,21 +51,23 @@ export default function AddInitiativeModal(props: AddInitiativeProps) {
           <p className='my-1'>Title</p>
           <input id='modalTitle' className={inputStyle + ' w-3/4'} onChange={(e) => {setInitiativeTitle(e.target.value)}}/>
           
-          <p className='mt-2'>Target Completion</p>
-          <div className='flex mb-2'>
-            <div>
-              <p>Month </p>
-              <input id='modalMonth' className={inputStyle + ' w-20 mx-1'} onChange={(e) => {setInitiativeTargetDate({...initiativeTargetDate, month: e.target.value})}}/>
-            </div>
-            
-            <div>
-              <p>Day </p>
-              <input id='modalDay' className={inputStyle + ' w-20 mx-1'} onChange={(e) => {setInitiativeTargetDate({...initiativeTargetDate, day: e.target.value})}}/>
-            </div>
+          <div className='outline'>
+            <p className='mt-2'>Target Completion</p>
+            <div className='flex mb-2 space-x-[5px]'>
+              <div>
+                <p>Month </p>
+                <input id='modalMonth' className={inputStyle + ' w-20 mx-1'} onChange={(e) => {setInitiativeTargetDate({...initiativeTargetDate, month: e.target.value})}}/>
+              </div>
 
-            <div>
-              <p>Year </p>        
-              <input id='modalYear' className={inputStyle + ' w-20 mx-1'} onChange={(e) => {setInitiativeTargetDate({...initiativeTargetDate, year: e.target.value})}}/>
+              <div>
+                <p>Day </p>
+                <input id='modalDay' className={inputStyle + ' w-20 mx-1'} onChange={(e) => {setInitiativeTargetDate({...initiativeTargetDate, day: e.target.value})}}/>
+              </div>
+
+              <div>
+                <p>Year </p>        
+                <input id='modalYear' className={inputStyle + ' w-20 mx-1'} onChange={(e) => {setInitiativeTargetDate({...initiativeTargetDate, year: e.target.value})}}/>
+              </div>
             </div>
           </div>
 
