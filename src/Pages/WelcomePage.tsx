e2044379a41e2f048de5081d8f54a3b15cc9da07
Julian Garver <julian.garver@integrityinspired.com>
2023-02-24 15:58:40 -0600

    small style change

--- src/Pages/WelcomePage.tsx
+++ src/Pages/WelcomePage.tsx
@@ -1,6 +1,6 @@
 export default function WelcomePage(){
   return(
-    <div>
+    <div className="m-5">
       <p className="flex justify-center text-3xl">Welcome to the Integrity Inspired Solutions Client Portal!</p>
       <p className="flex justify-center text-2xl">To view the information on your project, please log in</p>
     </div>
