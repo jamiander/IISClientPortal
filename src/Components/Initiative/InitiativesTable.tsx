9b4ad52aacc1152bb91c6b7ba3b6adc99c93cff3
Julian Garver <julian.garver@integrityinspired.com>
2023-02-27 09:04:16 -0600

    simplified if branches

--- src/Components/Initiative/InitiativesTable.tsx
+++ src/Components/Initiative/InitiativesTable.tsx
@@ -28,127 +28,43 @@ export default function InitiativesTable(props: InitiativesProps) {
     }
   }, []);
   
-  if(props.radioStatus === 'active'){
-    return (
-      <table className="table-auto w-[100%] outline outline-3">
-        <thead className="outline outline-1">
-          <tr>
-            <th>Id</th>
-            <th>Title</th>
-            <th hidden={isCompanyHidden}>Company</th>
-            <th>Target Completion</th>
-            <th>Total Items</th>
-            <th>Items Remaining</th>
-            <th>Probability</th>
-          </tr>
-        </thead>
-        <tbody>
-          {
-            companies.map((company, index) => {
-              return (
-                InitiativesFilter(company.init, props.radioStatus).map((initiative, index) => {
-                  const itemsCompleted = initiative.itemsCompletedOnDate.map((item) => item.itemsCompleted);
-                  var total = 0;
-                  itemsCompleted.forEach((num) => total += num);
-                  return (
-                    <tr key={index}>
-                      <td className={tableDataStyle}>{initiative.id}</td>
-                      <td className={tableDataStyle}>{initiative.title}</td>
-                      <td className={tableDataStyle} hidden={isCompanyHidden}>{company.name}</td>
-                      <td className={tableDataStyle}>{initiative.targetDate.month + "/" + initiative.targetDate.day + "/" + initiative.targetDate.year}</td>
-                      <td className={tableDataStyle}>{initiative.totalItems}</td>
-                      <td className={tableDataStyle}>{initiative.totalItems - total}</td>
-                      <td></td>
-                    </tr>
-                  )
-                })
-              )
-            })
-          }
-        </tbody>
-      </table>
-    )
-  }
-  else if(props.radioStatus === 'inactive'){
-    return(
-      <table className="table-auto w-[100%] outline outline-3">
-        <thead className="outline outline-1">
-          <tr>
-            <th>Id</th>
-            <th>Title</th>
-            <th hidden={isCompanyHidden}>Company</th>
-            <th>Target Completion</th>
-            <th>Total Items</th>
-            <th>Items Remaining</th>
-            <th>Probability</th>
-          </tr>
-        </thead>
-        <tbody>
-          {
-            companies.map((company, index) => {
-              return (
-                InitiativesFilter(company.init, props.radioStatus).map((initiative, index) => {
-                  const itemsCompleted = initiative.itemsCompletedOnDate.map((item) => item.itemsCompleted);
-                  var total = 0;
-                  itemsCompleted.forEach((num) => total += num);
-                  return (
-                    <tr key={index}>
-                      <td className={tableDataStyle}>{initiative.id}</td>
-                      <td className={tableDataStyle}>{initiative.title}</td>
-                      <td className={tableDataStyle} hidden={isCompanyHidden}>{company.name}</td>
-                      <td className={tableDataStyle}>{initiative.targetDate.month + "/" + initiative.targetDate.day + "/" + initiative.targetDate.year}</td>
-                      <td className={tableDataStyle}>{initiative.totalItems}</td>
-                      <td className={tableDataStyle}>{initiative.totalItems - total}</td>
-                      <td></td>
-                    </tr>
-                  )
-                })
-              )
-            })
-          }
-        </tbody>
-      </table>
-    )
-  }
-  else{
-    return(
-      <table className="table-auto w-[100%] outline outline-3">
-        <thead className="outline outline-1">
-          <tr>
-            <th>Id</th>
-            <th>Title</th>
-            <th hidden={isCompanyHidden}>Company</th>
-            <th>Target Completion</th>
-            <th>Total Items</th>
-            <th>Items Remaining</th>
-            <th>Probability</th>
-          </tr>
-        </thead>
-        <tbody>
-          {
-            companies.map((company, index) => {
-              return (
-                company.init.map((initiative, index) => {
-                  const itemsCompleted = initiative.itemsCompletedOnDate.map((item) => item.itemsCompleted);
-                  var total = 0;
-                  itemsCompleted.forEach((num) => total += num);
-                  return (
-                    <tr key={index}>
-                      <td className={tableDataStyle}>{initiative.id}</td>
-                      <td className={tableDataStyle}>{initiative.title}</td>
-                      <td className={tableDataStyle} hidden={isCompanyHidden}>{company.name}</td>
-                      <td className={tableDataStyle}>{initiative.targetDate.month + "/" + initiative.targetDate.day + "/" + initiative.targetDate.year}</td>
-                      <td className={tableDataStyle}>{initiative.totalItems}</td>
-                      <td className={tableDataStyle}>{initiative.totalItems - total}</td>
-                      <td></td>
-                    </tr>
-                  )
-                })
-              )
-            })
-          }
-        </tbody>
-      </table>
-    )
-  }
+  return (
+    <table className="table-auto w-[100%] outline outline-3">
+      <thead className="outline outline-1">
+        <tr>
+          <th>Id</th>
+          <th>Title</th>
+          <th hidden={isCompanyHidden}>Company</th>
+          <th>Target Completion</th>
+          <th>Total Items</th>
+          <th>Items Remaining</th>
+          <th>Probability</th>
+        </tr>
+      </thead>
+      <tbody>
+        {
+          companies.map((company, index) => {
+            return (
+              (props.radioStatus !== 'all' ? InitiativesFilter(company.init, props.radioStatus) : company.init).map((initiative, index) => {
+                const itemsCompleted = initiative.itemsCompletedOnDate.map((item) => item.itemsCompleted);
+                var total = 0;
+                itemsCompleted.forEach((num) => total += num);
+                return (
+                  <tr key={index}>
+                    <td className={tableDataStyle}>{initiative.id}</td>
+                    <td className={tableDataStyle}>{initiative.title}</td>
+                    <td className={tableDataStyle} hidden={isCompanyHidden}>{company.name}</td>
+                    <td className={tableDataStyle}>{initiative.targetDate.month + "/" + initiative.targetDate.day + "/" + initiative.targetDate.year}</td>
+                    <td className={tableDataStyle}>{initiative.totalItems}</td>
+                    <td className={tableDataStyle}>{initiative.totalItems - total}</td>
+                    <td></td>
+                  </tr>
+                )
+              })
+            )
+          })
+        }
+      </tbody>
+    </table>
+  )
 }
