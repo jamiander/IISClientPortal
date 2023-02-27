3e6d7de6136faaec141430012ca5232568dbc940
Ryan Boyce <86334509+ryan-boyce@users.noreply.github.com>
2023-02-27 10:03:10 -0600

    Put edit initiative button into component

--- src/Components/EditInitiatveButton.tsx
+++ src/Components/EditInitiatveButton.tsx
@@ -9,12 +9,12 @@ interface EditInitiativeButtonProps {
 
 export function EditInitiativeButton(props: EditInitiativeButtonProps){
     return (
-        <div key={props.index} className={'py-1 flex self-end'}>
-            <button className=" mx-2 bg-[#21345b] text-sm text-white w-full h-8 rounded-md outline"
-                onClick={() => props.handleEditInitiative(props.company, props.initiative)}
-            >
-                Edit Initiative
-            </button>
-        </div>
+      <div key={props.index} className={'py-1 flex self-end'}>
+        <button id={"editInitiativeButton"+props.initiative.id} className=" mx-2 bg-[#21345b] hover:bg-[#445362] text-sm text-white w-full h-8 rounded-md outline"
+            onClick={() => props.handleEditInitiative(props.company, props.initiative)}
+        >
+            Edit Initiative
+        </button>
+      </div>
     );
 }
