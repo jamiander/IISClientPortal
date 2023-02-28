20a928070e6b349b03a5cb35840c186c18dd39e4
cnielson912 <cnielson912@gmail.com>
2023-02-28 10:28:36 -0600

    admin page separated into tabs

--- src/Pages/AdminPage.tsx
+++ src/Pages/AdminPage.tsx
@@ -1,4 +1,4 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import ManageUsersDisplay from "../Components/User/ManageUsersDisplay";
 import ManageInitiativesDisplay from "../Components/Initiative/ManageInitiativesDisplay";
@@ -10,6 +10,7 @@ export default function AdminPage(){
   const dispatch = useAppDispatch();
   const currentUser = useAppSelector(selectCurrentUser);
   const navigate = useNavigate();
+  const [table,setTable] = useState("clients");
   
   useEffect(() => {
     let kickThemOut = true;
@@ -32,13 +33,17 @@ export default function AdminPage(){
   return(
     <div className="my-[1%] mx-[2%] grid grid-cols-4">
     
-      <div className="col-span-4 mb-4 h-fit">
+      <div className="col-span-3 mb-4 h-fit">
         <p className="text-5xl">Admin Page</p>
       </div>
+      <div className="col-span-1 mb-4 h-fit flex justify-end space-x-3">
+        <button className="outline bg-[#21345b] text-white h-12 w-[40%] rounded-md hover:outline-[#2ed7c3] hover:text-[#2ed7c3]" onClick={()=>setTable("clients")}>Clients</button>
+        <button className="outline bg-[#21345b] text-white h-12 w-[40%] rounded-md hover:outline-[#2ed7c3] hover:text-[#2ed7c3]" onClick={()=>setTable("initiatives")}>Initiatives</button>
+      </div>
   
-      <ManageUsersDisplay />
+      {table === "clients" && <ManageUsersDisplay/>}
 
-      <ManageInitiativesDisplay />
+      {table === "initiatives" && <ManageInitiativesDisplay/>}
     </div>
   )
 }
