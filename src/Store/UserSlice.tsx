13718c2f771282166a0a6534e36375112b62cfc0
Julian Garver <julian.garver@integrityinspired.com>
2023-03-01 13:22:59 -0600

    remove unused code

--- src/Store/UserSlice.tsx
+++ src/Store/UserSlice.tsx
@@ -1,5 +1,4 @@
-import { createAsyncThunk, createSlice, PayloadAction } from "@reduxjs/toolkit";
-import { /*AddUser, GetAllUsers*/ } from "../Services/UserService";
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
 import { RootState } from "./Store";
 
 export interface User {
@@ -20,34 +19,6 @@ const initialState: UserState = {
     users: [],
     currentUserId: -1
 }
-
-/*export const getUserData = createAsyncThunk(
-    'users/getUserData',
-    async () => {
-        const response = await GetAllUsers();
-        return response;
-    }
-)
-
-interface AddUserArgs {
-    user: User,
-    isTest: boolean
-}
-
-export const addUser = createAsyncThunk(
-    'users/addUser',
-    async (args: AddUserArgs) : Promise<User> => {
-        const response = await AddUser(args);
-        const status = response.status;
-        console.log(status);
-        if(status.toUpperCase().includes('FAILED'))
-            throw Error;
-        
-        let newUser = JSON.parse(JSON.stringify(args.user));
-        newUser.id = response.id;
-        return newUser;
-    }
-)*/
 
 export const userSlice = createSlice({
     name: "users",
@@ -75,19 +46,7 @@ export const userSlice = createSlice({
             }
         }
     },
-    extraReducers: (builder) => {
-        //builder
-            /*.addCase(getUserData.fulfilled, (state, action) => {
-                state.users = action.payload;
-            })
-            .addCase(addUser.fulfilled, (state, action) => {
-                let existingUser = state.users.find(user => user.id === action.payload.id);
-                if(!existingUser)
-                    state.users.push(action.payload);
-                else
-                    existingUser = action.payload;
-            })*/
-    },
+    extraReducers: (builder) => {},
 });
 
 export const { setCurrentUserId, signOut, addUsersToStore } = userSlice.actions;
