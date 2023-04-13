d5be1539f2dafdf2a99d8bc241c07ded836cb770
Ryan Boyce <86334509+ryan-boyce@users.noreply.github.com>
2023-04-13 14:02:07 -0500

    Change tabs to not look like buttons

--- src/Pages/AdminPage.tsx
+++ src/Pages/AdminPage.tsx
@@ -6,13 +6,16 @@ export default function AdminPage(){
   
   const [table,setTable] = useState("initiatives");
 
+  const tabStyle = "outline outline-white text-white bg-[#21345b] h-12 w-1/5 rounded-t-lg hover:outline-[#2ed7c3] hover:text-[#2ed7c3]";
+  const selectedTabStyle = tabStyle.replace("text-white","text-black").replace("bg-[#21345b]","bg-[#2ed7c3]").replace("hover:outline-[#2ed7c3] hover:text-[#2ed7c3]","hover:text-black");
+
   return(
     <div className="my-[1%] mx-[2%] grid grid-cols-4">
       <div className="flex col-span-4 mb-4 h-fit bg-[#2ed7c3] rounded-md py-6 px-5">
         <p className="text-5xl w-1/4">Admin</p>
         <div className="h-fit w-3/4 space-x-5 flex justify-end">
-          <button className="outline outline-white text-white bg-[#21345b] h-12 w-1/5 rounded-md hover:outline-[#2ed7c3] hover:text-[#2ed7c3]" onClick={()=>setTable("clients")}>Clients</button>
-          <button className="outline outline-white text-white bg-[#21345b] h-12 w-1/5 rounded-md hover:outline-[#2ed7c3] hover:text-[#2ed7c3]" onClick={()=>setTable("initiatives")}>Initiatives</button>
+          <button className={table === "clients" ? selectedTabStyle : tabStyle} onClick={()=>setTable("clients")}>Clients</button>
+          <button className={table === "initiatives" ? selectedTabStyle : tabStyle} onClick={()=>setTable("initiatives")}>Initiatives</button>
         </div>
       </div>
   
