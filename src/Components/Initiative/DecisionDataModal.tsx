9227b39a5230a4ce3c710501c666e5cbe998db4b
Ryan Boyce <86334509+ryan-boyce@users.noreply.github.com>
2023-04-27 11:29:34 -0500

    Add edit button

--- src/Components/Initiative/DecisionDataModal.tsx
+++ src/Components/Initiative/DecisionDataModal.tsx
@@ -1,17 +1,16 @@
 import Button from "@mui/material/Button";
 import CardActions from "@mui/material/CardActions";
 import Dialog from "@mui/material/Dialog";
-import Typography from "@mui/material/Typography";
 import { Company, Initiative } from "../../Store/CompanySlice";
 import Grid from "@mui/material/Grid";
 import Paper from "@mui/material/Paper";
 import { styled } from '@mui/material/styles';
-import CssBaseline from "@mui/material/CssBaseline";
-import { Fragment, RefObject, createRef, useEffect, useRef, useState } from "react";
+import { useEffect, useState } from "react";
 import { cancelButtonStyle, submitButtonStyle } from "../../Styles";
 import { DateInfo, DecisionData } from "../../Services/CompanyService";
 import TextField from "@mui/material/TextField";
 import { Card, CardContent } from "@mui/material";
+import { MakeDateInfo, MakeDateString } from "../DateInput";
 
 export const DecisionModalIds = {
   modal: "decisionModal",
@@ -39,23 +38,28 @@ interface DecisionDataProps {
 
 export default function DecisionDataModal(props: DecisionDataProps) {
   const [currentDescription, setCurrentDescription] = useState("");
-  
-
+  const [currentResolution, setCurrentResolution] = useState("");
+  const [currentParticipants, setCurrentParticipants] = useState("");
+  const [currentDate, setCurrentDate] = useState<DateInfo>();
 
   const [selectedInitiative, setSelectedInitiative] = useState<Initiative>(props.initiative);
+  const [editIndex, setEditIndex] = useState(-1);
 
   useEffect(() => {
     setSelectedInitiative(props.initiative);
+    setEditIndex(-1);
   },[props.isOpen])
 
 
-  function EditDecision(key: number, newDescription: string, newResolution: string, newParticipants: string[], newDate: DateInfo) {
-    let selectedInitiativeClone:Initiative = JSON.parse(JSON.stringify(selectedInitiative));
-
-    selectedInitiativeClone.decisions[key].description = newDescription;
-    selectedInitiativeClone.decisions[key].resolution = newResolution;
-    selectedInitiativeClone.decisions[key].participants = newParticipants;
-    selectedInitiativeClone.decisions[key].date = newDate;
+  function EditDecision(key: number, newDescription: string, newResolution: string, newParticipants: string[], newDate?: DateInfo) {
+    let selectedInitiativeClone: Initiative = JSON.parse(JSON.stringify(selectedInitiative));
+    let newDecision = selectedInitiativeClone.decisions[key];
+    
+    newDecision.description = newDescription;
+    newDecision.resolution = newResolution;
+    newDecision.participants = newParticipants;
+    if(newDate)
+      newDecision.date = newDate;
 
     setSelectedInitiative(selectedInitiativeClone);
   }
@@ -75,20 +79,31 @@ export default function DecisionDataModal(props: DecisionDataProps) {
             <Grid container spacing={4}>
               {
               selectedInitiative.decisions.map((displayItem, key) => {
+                let isEdit = (key === editIndex);
+                
               return(
                 <Grid item md={4} key={key}>
                   <Item>
                     <Card>
                       <CardContent>
                         <TextField value={currentDescription} onChange={e => setCurrentDescription(e.target.value)}></TextField>
-                        <TextField defaultValue={displayItem.resolution}></TextField>
-                        <TextField defaultValue={displayItem.participants}></TextField>
-                        <TextField type="date" defaultValue={displayItem.date}></TextField>
+                        <TextField value={currentResolution} onChange={e => setCurrentResolution(e.target.value)}></TextField>
+                        <TextField value={currentParticipants} onChange={e => setCurrentParticipants(e.target.value)}></TextField>
+                        <TextField type="date" value={currentDate ? MakeDateString(currentDate) : ""} onChange={e => setCurrentDate(MakeDateInfo(e.target.value))}></TextField>
                       </CardContent>
                       <CardActions>
-                        <Button>Share</Button>
-                        <button className={submitButtonStyle} onClick={() => EditDecision(key, currentDescription, displayItem.resolution, displayItem.participants, displayItem.date)}>Save
-                        </button>
+                        {isEdit &&
+                          <div className="flex w-full justify-between">
+                            <button className={submitButtonStyle} onClick={() => EditDecision(key, currentDescription, currentResolution, currentParticipants.split(","), currentDate)}>Save</button>
+                            <button className={cancelButtonStyle} onClick={() => setEditIndex(-1)}>Cancel</button>
+                          </div>
+                        }
+                        {
+                          !isEdit &&
+                          <div className="flex w-full justify-start">
+                            <button className={submitButtonStyle} onClick={() => setEditIndex(key)}>Edit</button>
+                          </div>
+                        }
                       </CardActions>
                     </Card>
                   </Item>
