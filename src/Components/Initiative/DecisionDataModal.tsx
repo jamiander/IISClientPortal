6dc6c7b04da0ae31e5a12c071a8dabfac089e0a3
Ryan Boyce <86334509+ryan-boyce@users.noreply.github.com>
2023-04-28 12:16:23 -0500

    Add search bar to decision modal

--- src/Components/Initiative/DecisionDataModal.tsx
+++ src/Components/Initiative/DecisionDataModal.tsx
@@ -2,7 +2,7 @@ import Dialog from "@mui/material/Dialog";
 import { Company, Initiative, deleteDecisionData, updateDecisionData } from "../../Store/CompanySlice";
 import Grid from "@mui/material/Grid";
 import { useEffect, useState } from "react";
-import { Item, StyledCard, StyledCardActions, StyledCardContent, StyledTextField, StyledTextarea, cancelButtonStyle, genericButtonStyle, submitButtonStyle, yellowButtonStyle, labelStyle } from "../../Styles";
+import { Item, StyledCard, StyledCardActions, StyledCardContent, StyledTextField, StyledTextarea, cancelButtonStyle, genericButtonStyle, submitButtonStyle, yellowButtonStyle, labelStyle, inputStyle } from "../../Styles";
 import { DateInfo, DecisionData } from "../../Services/CompanyService";
 import { Button } from "@mui/material";
 import { MakeDateInfo, MakeDateString } from "../DateInput";
@@ -40,10 +40,13 @@ interface DecisionDataProps {
     const [currentDateString, setCurrentDateString] = useState("");
   
     const [selectedInitiative, setSelectedInitiative] = useState<Initiative>(props.initiative);
-    const [editIndex, setEditIndex] = useState(-1);
+    const [decisionToEdit, setDecisionToEdit] = useState<DecisionData>();
     const [isNew, setIsNew] = useState(false);
     const [isDeleteOpen, setIsDeleteOpen] = useState(false);
     const [decisionToDelete, setDecisionToDelete] = useState<DecisionData>();
+    const InEditMode = () => decisionToEdit !== undefined;
+
+    const [searchedKeyword, setSearchedKeyword] = useState("");
 
   useEffect(() => {
     setSelectedInitiative(props.initiative);
@@ -60,22 +63,25 @@ interface DecisionDataProps {
 
     setSelectedInitiative(initiativeClone);
     setIsNew(true);
-    EnterEditMode(initiativeClone.decisions.length-1,initiativeClone);
+    EnterEditMode(negativeId,initiativeClone);
   }
 
-  function EnterEditMode(index: number, currentInitiative: Initiative)
+  function EnterEditMode(id: number, currentInitiative: Initiative)
   {
-    setEditIndex(index);
-    let currentDecision = currentInitiative.decisions[index];
-    setCurrentDescription(currentDecision.description);
-    setCurrentResolution(currentDecision.resolution);
-    setCurrentParticipants(currentDecision.participants.join(", "));
-    setCurrentDateString(MakeDateString(currentDecision.date));
+    let currentDecision = currentInitiative.decisions.find(d => d.id === id);
+    if(currentDecision)
+    {
+      setDecisionToEdit(currentDecision);
+      setCurrentDescription(currentDecision.description);
+      setCurrentResolution(currentDecision.resolution);
+      setCurrentParticipants(currentDecision.participants.join(", "));
+      setCurrentDateString(MakeDateString(currentDecision.date));
+    }
   }
 
   function LeaveEditMode()
   {
-    setEditIndex(-1);
+    setDecisionToEdit(undefined);
     setIsNew(false);
   }
 
@@ -84,29 +90,32 @@ interface DecisionDataProps {
     if(isNew)
     {
       let initiativeClone: Initiative = JSON.parse(JSON.stringify(selectedInitiative));
-      initiativeClone.decisions.splice(editIndex,1);
+      initiativeClone.decisions.filter(d => d.id !== (decisionToEdit?.id ?? -1));
 
       setSelectedInitiative(initiativeClone);
     }
     LeaveEditMode();
   }
 
-  function EditDecision(key: number, newDescription: string, newResolution: string, newParticipants: string[], newDate?: DateInfo)
+  function EditDecision(decisionId: number, newDescription: string, newResolution: string, newParticipants: string[], newDate?: DateInfo)
   {
     let selectedInitiativeClone: Initiative = JSON.parse(JSON.stringify(selectedInitiative));
-    let newDecision = selectedInitiativeClone.decisions[key];
+    let newDecision = selectedInitiativeClone.decisions.find(d => d.id === decisionId);
     
-    newDecision.description = newDescription;
-    newDecision.resolution = newResolution;
-    newDecision.participants = newParticipants;
-    if(newDate)
-      newDecision.date = newDate;
+    if(newDecision)
+    {
+      newDecision.description = newDescription;
+      newDecision.resolution = newResolution;
+      newDecision.participants = newParticipants;
+      if(newDate)
+        newDecision.date = newDate;
 
-    let successfulSubmit = SubmitDecisionData(selectedInitiativeClone.decisions);
-    if(successfulSubmit)
-      setSelectedInitiative(selectedInitiativeClone);
-    else
-      setSelectedInitiative(selectedInitiative);
+      let successfulSubmit = SubmitDecisionData(selectedInitiativeClone.decisions);
+      if(successfulSubmit)
+        setSelectedInitiative(selectedInitiativeClone);
+      else
+        setSelectedInitiative(selectedInitiative);
+    }
   }
 
   function SubmitDecisionData(decisions: DecisionData[]): boolean
@@ -128,10 +137,10 @@ interface DecisionDataProps {
     return false;
   }
 
-  function AttemptDelete(index: number)
+  function AttemptDelete(decisionId: number)
   {
     setIsDeleteOpen(true);
-    setDecisionToDelete(selectedInitiative.decisions[index]);
+    setDecisionToDelete(selectedInitiative.decisions.find(d => d.id === decisionId));
   }
 
   function DeleteDecision(decisionId: number)
@@ -161,14 +170,15 @@ interface DecisionDataProps {
           <div className="flex col-span-4 mb-4 bg-[#2ed7c3] rounded-md py-6 px-5">
             <div className="w-full flex justify-between">
             <p className="text-5xl font-bold w-1/4">{props.company.name} : {props.initiative.title}</p>
-            <button disabled={editIndex !== -1} id={DecisionModalIds.addButton} className={yellowButtonStyle} onClick={() => AddEmptyDecision()}>Add Decision</button>
+            <button disabled={InEditMode()} id={DecisionModalIds.addButton} className={yellowButtonStyle} onClick={() => AddEmptyDecision()}>Add Decision</button>
             </div>
           </div>
           <div style={{margin: '2%'}}>
+            <StyledTextField disabled={InEditMode()} label="Search" value={searchedKeyword} onChange={(e) => setSearchedKeyword(e.target.value)}/>
             <Grid container spacing={6}>
               {
-              selectedInitiative.decisions.map((displayItem, key) => {
-                let isEdit = (key === editIndex);
+              selectedInitiative.decisions.filter(d => d.description.toUpperCase().includes(searchedKeyword.toUpperCase()) || d.resolution.toUpperCase().includes(searchedKeyword.toUpperCase())).map((displayItem, key) => {
+                let isEdit = (displayItem.id === (decisionToEdit?.id ?? -1));
                 
               return(
                 <Grid item md={4} key={key}>
@@ -198,15 +208,15 @@ interface DecisionDataProps {
                       <StyledCardActions>
                         {isEdit &&
                           <div className="flex w-full justify-between">
-                            <button id={DecisionModalIds.saveChangesButton} className={submitButtonStyle} onClick={() => EditDecision(key, currentDescription, currentResolution, currentParticipants.split(", "), currentDateString ? MakeDateInfo(currentDateString) : displayItem.date)}>Save</button>
+                            <button id={DecisionModalIds.saveChangesButton} className={submitButtonStyle} onClick={() => EditDecision(displayItem.id, currentDescription, currentResolution, currentParticipants.split(", "), currentDateString ? MakeDateInfo(currentDateString) : displayItem.date)}>Save</button>
                             <button id={DecisionModalIds.cancelChangesButton} className={cancelButtonStyle} onClick={() => CancelEdit()}>Cancel</button>
                           </div>
                         }
                         {
-                          !isEdit && editIndex === -1 &&
+                          !isEdit && !InEditMode() &&
                           <div className="flex w-full justify-between">
-                            <button className={submitButtonStyle} onClick={() => EnterEditMode(key, selectedInitiative)}>Edit</button>
-                            <button className={cancelButtonStyle} onClick={() => AttemptDelete(key)}>Delete</button>
+                            <button className={submitButtonStyle} onClick={() => EnterEditMode(displayItem.id, selectedInitiative)}>Edit</button>
+                            <button className={cancelButtonStyle} onClick={() => AttemptDelete(displayItem.id)}>Delete</button>
                           </div>
                         }
                       </StyledCardActions>
