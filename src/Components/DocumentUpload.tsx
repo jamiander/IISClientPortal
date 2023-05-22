c376fa891d5a112b4aad0f7f008f619236b17827
Ryan Boyce <86334509+ryan-boyce@users.noreply.github.com>
2023-05-22 14:05:35 -0500

    Add service method for document downloading

--- src/Components/DocumentUpload.tsx
+++ src/Components/DocumentUpload.tsx
@@ -2,13 +2,14 @@ import { useState } from "react";
 import { yellowButtonStyle } from "../Styles";
 import { v4 } from "uuid";
 import { useAppDispatch } from "../Store/Hooks";
-import { getDocumentUrls, uploadDocuments } from "../Store/DocumentSlice";
+import { DocumentInfo, downloadDocument, getDocumentUrls, uploadDocument } from "../Store/DocumentSlice";
 import { IntegrityId } from "../Store/CompanySlice";
 
 export function DocumentUpload()
 {
   const dispatch = useAppDispatch();
   const [files, setFiles] = useState<FileList>();
+  const [docInfo, setDocInfo] = useState<DocumentInfo>();
 
   function HandleFile(newFiles: FileList | null)
   {
@@ -23,19 +24,34 @@ export function DocumentUpload()
     if(files)
     {
       let documentId = v4();
-      dispatch(uploadDocuments({files: files, companyId: IntegrityId, documentId: documentId, isTest: true}));
+      dispatch(uploadDocument({file: files[0], companyId: IntegrityId, documentId: documentId, isTest: true}));
+    }
+  }
+
+  async function HandleGet()
+  {
+    const docs = (await dispatch(getDocumentUrls({companyId: IntegrityId}))).payload as DocumentInfo[];
+    setDocInfo(docs[0]);
+  }
+
+  async function HandleDownload()
+  {
+    if(docInfo)
+    {
+      const result = await dispatch(downloadDocument({documentInfo: docInfo}))
     }
   }
 
   return (
     <>
-        <div>
-          <input type="file" onChange={(e) => HandleFile(e.target.files)}/>
-          <button className={yellowButtonStyle} onClick={() => HandleUpload()}>Upload</button>
-        </div>
-        <div>
-          <button className={yellowButtonStyle} onClick={() => dispatch(getDocumentUrls({companyId: IntegrityId}))}>Get Documents</button>
-        </div>
+      <div>
+        <input type="file" onChange={(e) => HandleFile(e.target.files)}/>
+        <button className={yellowButtonStyle} onClick={() => HandleUpload()}>Upload</button>
+      </div>
+      <div>
+        <button className={yellowButtonStyle} onClick={() => HandleGet()}>Get Documents</button>
+        <button className={yellowButtonStyle} onClick={() => HandleDownload()}>Download {docInfo?.name}</button>
+      </div>
     </>
   )
 }
