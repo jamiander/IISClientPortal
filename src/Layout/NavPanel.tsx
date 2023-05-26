c3b0259bb889b19a2b9ca09de78595f5b644820a
Jamie Andersen <jamie@integrityinspired.com>
2023-05-26 11:14:44 -0500

    open nav menu

--- src/Layout/NavPanel.tsx
+++ src/Layout/NavPanel.tsx
@@ -3,6 +3,8 @@ import { useAppSelector } from "../Store/Hooks"
 import { selectCurrentUser, selectIsLoggedIn } from "../Store/UserSlice"
 import { enqueueSnackbar } from "notistack"
 import { IntegrityId } from "../Store/CompanySlice"
+import { useState } from "react"
+import { Menu } from "@mui/material"
 
 export const NavPanelIds = {
   initiatives: "navPanelInitiatives",
@@ -17,13 +19,18 @@ export default function NavPanel(){
   const currentUser = useAppSelector(selectCurrentUser);
   const navButtonStyle = "text-[#21345b] h-12 w-[90%] hover:bg-[#21345b] hover:text-white";
   const location = useLocation();
+  const [navbarOpen, setNavbarOpen] = useState(false);
 
   function NavHandler(path: string) {
     if (!isLoggedIn) {
       
       enqueueSnackbar('You must login to leave this page', {variant:'error'});
     }
-    else navigate(path);
+    else 
+    {
+      navigate(path);
+      setNavbarOpen(false);
+    }
   }
 
   function IsActivePath(pathToCheck: string)
@@ -38,6 +45,8 @@ export default function NavPanel(){
 
   return(
     <div className="grid place-items-left p-[2%] py-3 space-y-3">
+      <button onClick={() => setNavbarOpen(!navbarOpen)}>{navbarOpen ? 'close' : 'open'}</button>
+      <Menu className={navbarOpen ? 'menuStyle' : 'closeMenuStyle'} open={navbarOpen}>
       <button style={{ fontSize: '20px', marginTop: '30px', marginLeft: '10px', textAlign: "left"}} id={NavPanelIds.initiatives} className={GetNavStyle("/Initiatives")}
         onClick={() => NavHandler('/Initiatives')}>
           <i className="material-icons" style={{ fontSize: '30px', marginRight: '25px'}}>info_outline</i>
@@ -67,6 +76,7 @@ export default function NavPanel(){
           Client Management
         </button>
       }
+    </Menu>
     </div>  
   )
 }
