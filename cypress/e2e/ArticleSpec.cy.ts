0c9a0646541ea3b132f24ec1aa121cac565f8690
Ryan Boyce <86334509+ryan-boyce@users.noreply.github.com>
2023-06-19 11:05:34 -0500

    Create test function for adding/editing articles

--- cypress/e2e/ArticleSpec.cy.ts
+++ cypress/e2e/ArticleSpec.cy.ts
@@ -1,9 +1,29 @@
-import { AdminUser, IntegrityUser, MBPIAdminUser, MBPIUser } from "./TestHelpers";
+import { Article } from "../../src/Store/ArticleSlice";
+import { AdminUser, IntegrityUser, MBPIAdminUser, MBPIUser, TestConstants } from "./TestHelpers";
 
 const integrityUser = IntegrityUser;
 const integrityAdmin = AdminUser;
 const clientUser = MBPIUser;
 const clientAdmin = MBPIAdminUser;
+
+const consts = TestConstants;
+const initTableIds = consts.initiativeTableIds;
+const clientPageIds = consts.clientPageIds;
+const modalIds = consts.articleModalIds;
+
+const articleToAdd = {
+  title: "New Article",
+  text: "Hello. This is my new article.",
+  updatedDate: "01012023",
+  updatedBy: "Johnny Test",
+}
+
+const editedArticle = {
+  title: "Edited Article",
+  text: "Goodbye.",
+  updatedDate: "01022023",
+  updatedBy: "Jimmy Toast"
+}
 
 function GoToClientArticles()
 {
@@ -12,27 +32,52 @@ function GoToClientArticles()
 
 function GoToInitiativeArticles()
 {
-
+  cy.getByData(initTableIds.actionMenu.menuButton).first().click();
+  cy.getByData(initTableIds.actionMenu.articleButton).click();
 }
 
 function AddArticle()
 {
+  cy.getByData(modalIds.addButton).click();
+  cy.getByData(modalIds.editTitle).type(articleToAdd.title);
+  cy.getByData(modalIds.editText).type(articleToAdd.text);
+  cy.getByData(modalIds.editUpdatedBy).type(articleToAdd.updatedBy);
+  cy.getByData(modalIds.editUpdatedDate).setDatePicker(articleToAdd.updatedDate);
+  cy.getByData(modalIds.saveChangesButton).click();
 
+  cy.getByData(modalIds.saveChangesButton).should('not.exist');
+  cy.contains(articleToAdd.title).parent().within(() => {
+    cy.getByData(modalIds.text).should('contain',articleToAdd.text);
+    cy.getByData(modalIds.updatedBy).should('contain',articleToAdd.updatedBy);
+    //cy.getByData(modalIds.updatedDate).should('contain',articleToAdd.updatedDate);
+  });
 }
 
 function EditArticle()
 {
+  cy.getByData(modalIds.editButton).first().click();
+  cy.getByData(modalIds.editTitle).clear().type(editedArticle.title);
+  cy.getByData(modalIds.editText).clear().type(editedArticle.text);
+  cy.getByData(modalIds.editUpdatedBy).clear().type(editedArticle.updatedBy);
+  cy.getByData(modalIds.editUpdatedDate).setDatePicker(editedArticle.updatedDate);
+  cy.getByData(modalIds.saveChangesButton).click();
 
+  cy.getByData(modalIds.saveChangesButton).should('not.exist');
+  cy.contains(editedArticle.title).parent().within(() => {
+    cy.getByData(modalIds.text).should('contain',editedArticle.text);
+    cy.getByData(modalIds.updatedBy).should('contain',editedArticle.updatedBy);
+    //cy.getByData(modalIds.updatedDate).should('contain',articleToAdd.updatedDate);
+  });
 }
 
 function CannotAdd()
 {
-
+  cy.getByData(modalIds.addButton).should('not.exist');
 }
 
 function CannotEdit()
 {
-
+  cy.getByData(modalIds.editButton).should('not.exist');
 }
 
 function CannotAddInvalidArticle()
@@ -106,9 +151,10 @@ describe('add client-level article', () => {
   
 });
 
-describe('add initiative-level article', () => {
+describe.only('add initiative-level article', () => {
 
   specify('Client users cannot add/edit articles', () => {
+    //TODO: for editing, we need to ensure an article is there in the first place
     cy.login(clientUser);
     GoToInitiativeArticles();
     CannotAdd();
@@ -116,6 +162,7 @@ describe('add initiative-level article', () => {
   })
 
   specify('Client admins cannot add/edit articles', () => {
+    //TODO: same here
     cy.login(clientAdmin);
     GoToInitiativeArticles();
     CannotAdd();
@@ -129,7 +176,7 @@ describe('add initiative-level article', () => {
     EditArticle();
   })
 
-  specify('Integrity admins can add/edit articles', () => {
+  specify.only('Integrity admins can add/edit articles', () => {
     cy.login(integrityAdmin);
     GoToInitiativeArticles();
     AddArticle();
